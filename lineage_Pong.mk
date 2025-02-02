8bc7786829488787c97fa26c30a047a1bf6c0a17
Ghosuto <clash.raja10@gmail.com>
2025-02-02 13:18:15 +0000

    Pong: Matrixx bringup
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- lineage_Pong.mk
+++ lineage_Pong.mk
@@ -24,6 +24,19 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
+# To include Gapps 
+WITH_GMS := true
+TARGET_USE_REMOVEPACKAGE  := true
+
+# Device has official
+MATRIXX_BUILD_TYPE := Official
+
+# Udfps icons
+TARGET_HAS_UDFPS := true
+
+#Some more GMS Flag
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="Pong-user 12 SKQ1.240903.001 2501131723 release-keys" \
     BuildFingerprint=Nothing/Pong/Pong:12/SKQ1.240903.001/2501131723:user/release-keys \
